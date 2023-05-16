b8b49bd221bb833d86d647967f5220e70ae4c8a0
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2023-05-15 22:32:18 -0300

    some CSS Updates

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,298 +1,282 @@
-const produtos = [{
-        link: "https://shopee.com.br/Caneca-How-I-Met-Your-Mother-Barney's-Teaching-i.390189578.9126962141",
-        tag: "HIMYM",
-        nome: "Barney's teaching",
-        imagem: "Barneys teaching-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/product/390189578/10915871465/",
-        tag: "Marvel Comics",
-        nome: "Spider Man Photographer",
-        imagem: "spider-photo-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-How-I-Met-Your-Mother-All-I-Had-To-Do-i.390189578.4582160451",
-        tag: "HIMYM",
-        nome: "All I had to do",
-        imagem: "All-I-had-to-do-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Among-Us-H%C3%A1-01-Impostor-entre-n%C3%B3s...-i.390189578.9735647854",
-        tag: "Among Us",
-        nome: "Há 01 Impostor entre nós...",
-        imagem: "amongus_1-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Harry-Potter-Hogwarts-Aquarela-i.390189578.3387575216",
-        tag: "Harry Potter",
-        nome: "Hogwarts Aquarela",
-        imagem: "aquarela-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-The-Mandalorian-Strong-in-me-Cuteness-is!-i.390189578.6979625519",
-        tag: "Star Wars",
-        nome: "Strong in me Cuteness is!",
-        imagem: "Baby-Yoda-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-LOST-D.H.A.R.M.A-(S%C3%A9rie-LOST-DHARMA)-i.390189578.8926974734",
-        tag: "LOST",
-        nome: "Iniciativa DHARMA",
-        imagem: "Dharma-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Harry-Potter-Expresso-Patronum!-i.390189578.2919122088",
-        tag: "Harry Potter",
-        nome: "Expresso Patronum!",
-        imagem: "Espresso-Patronum-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Friends-Friends-2-(S%C3%A9rie-Friends)-i.390189578.5082159713",
-        tag: "Friends",
-        nome: "Friends #2<br><br>",
-        imagem: "Friends_1-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Friends-Friends-Like-(S%C3%A9rie-Friends)-i.390189578.8326958121",
-        tag: "Friends",
-        nome: "Friends Like<br><br>",
-        imagem: "Friends-Like-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Friends-Friends-1-(S%C3%A9rie-Friends)-i.390189578.9026957664",
-        tag: "Friends",
-        nome: "Friends #1<br><br>",
-        imagem: "Friends-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Dragon-Ball-Son-Goku-1-i.390189578.3282295521",
-        tag: "Dragon Ball",
-        nome: "Son Goku #1<br><br>",
-        imagem: "goku-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Grey's-Anatomy-Grey's-Anatomy-2-i.390189578.9826959714",
-        tag: "Grey's Anatomy",
-        nome: "Grey's Anatomy #2",
-        imagem: "Greys-Anatomy2-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Grey's-Anatomy-Grey's-Anatomy-1-i.390189578.4282160527",
-        tag: "Grey's Anatomy",
-        nome: "Grey's Anatomy #1",
-        imagem: "Greys-Anatomy-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Rick-and-Morty-I-Can-Prove-It-Mathematically-i.390189578.9226960400",
-        tag: "Rick and Morty",
-        nome: " I Can Prove It Mathematically",
-        imagem: "I-can-prove-it-mathematically-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Grey's-Anatomy-It's-a-Beautiful-Day-To-Save-Lives-i.390189578.4182165680",
-        tag: "Grey's Anatomy",
-        nome: "It's a Beautiful Day",
-        imagem: "Its-a-beutiful-day-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-DC-Comics-Justice-League-1-i.390189578.3186244862",
-        tag: "DC Comics",
-        nome: "Justice League #1",
-        imagem: "Justice-League-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Naruto-Kakashi-Hatake-1-i.390189578.8826985815",
-        tag: "Naruto",
-        nome: "Kakashi Hatake #1",
-        imagem: "Kakashi-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Dragon-Ball-Kame-House-i.390189578.6782160403",
-        tag: "Dragon Ball",
-        nome: "Kame House<br><br>",
-        imagem: "Kame-House-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Naruto-Lend%C3%A1rio-Jiraya-i.390189578.8926985904",
-        tag: "Naruto",
-        nome: "Lendário Jiraya",
-        imagem: "Lendário-Jiraya-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-LOST-LOST-(S%C3%A9rie-de-LOST)-i.390189578.8026974805",
-        tag: "LOST",
-        nome: "LOST<br><br>",
-        imagem: "LOST-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Rick-and-Morty-Mr.-Meeseeks-i.390189578.9626954854",
-        tag: "Rick and Morty",
-        nome: "Mr. Meeseeks<br><br>",
-        imagem: "mrMeesekes-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Rick-and-Morty-Mug-Riiiiick!-i.390189578.5879625606",
-        tag: "Rick and Morty",
-        nome: " Mug Riiiiick!<br><br>",
-        imagem: "Mug-Morty-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Naruto-Naruto-Evolution-i.390189578.8826984665",
-        tag: "Naruto",
-        nome: "Naruto Evolution",
-        imagem: "Naruto-Evolution-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Star-Wars-New-Trilogy-i.390189578.3016920469",
-        tag: "Star Wars",
-        nome: "New Trilogy<br><br>",
-        imagem: "New-Triology-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Dev-No-Coffe-No-Code-(Caneca-para-Desenvolvedores)-i.390189578.7985946610",
-        tag: "Dev",
-        nome: "No Coffe No Code",
-        imagem: "NoCoffeNoCode-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-LOST-Not-Penny's-Boatt-i.390189578.8926976626",
-        tag: "LOST",
-        nome: "Not Penny's Boat",
-        imagem: "Not-Penny's-Boat-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Harry-Potter-O-conto-dos-3-irm%C3%A3os-(Deathly-Hallows-Rel%C3%ADquias-da-Morte)-i.390189578.10200384827",
-        tag: "Harry Potter",
-        nome: "O conto dos 3 irmãos",
-        imagem: "o conto dos tres irmãos-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Dev-Programador-Criativo-i.390189578.7185951726",
-        tag: "Dev",
-        nome: "rogramador Criativo",
-        imagem: "ProgramadorCriativo-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Marvel-Comics-Homem-Aranha-Vintage-Comics-i.390189578.9216364978",
-        tag: "Marvel Comics",
-        nome: "Homem Aranha Vintage",
-        imagem: "Spider-Man-Vintage-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Marvel-Comics-Stark-Industries-i.390189578.8026980933",
-        tag: "Marvel Comics",
-        nome: "Stark Industries",
-        imagem: "Stark-Indrustries-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Star-Wars-Starbucks-Grogu-(Baby-Yoda-Starbucks)-i.390189578.8776815529",
-        tag: "Star Wars",
-        nome: "Starbucks Grogu",
-        imagem: "StartBuksGrogu01-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-DC-Comics-The-Flash-(S%C3%A9rie-Flash)-i.390189578.2929386534",
-        tag: "DC Comics",
-        nome: "The Flash<br><br>",
-        imagem: "The-Flash-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Dragon-Ball-Personagens-1-i.390189578.8426978473",
-        tag: "Dragon Ball",
-        nome: "Personagens #1<br><br>",
-        imagem: "Todos-os-personagens-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Harry-Potter-Os-tr%C3%AAs-amigos-i.390189578.5687479464",
-        tag: "Harry Potter",
-        nome: "Os três amigos<br><br>",
-        imagem: "TrioAmigos-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Star-Wars-Darth-Troopers-i.390189578.9716364566",
-        tag: "Star Wars",
-        nome: "Darth Troopers<br><br>",
-        imagem: "Troopers-min .jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/product/390189578/8776822933/",
-        tag: "Star Wars",
-        nome: "HATE<br><br>",
-        imagem: "Hate-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-One-Piece-Trafalgar-Law-i.390189578.11417557325",
-        tag: "One Piece",
-        nome: "Trafalgar Law<br><br>",
-        imagem: "trafalgar-law2-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Star-Wars-St%C3%A1U%C3%B3-i.390189578.10317554557",
-        tag: "Star Wars",
-        nome: "Stá Uó<br><br>",
-        imagem: "StaUó-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Star-Wars-A-New-Hope-(Uma-nova-esperan%C3%A7a)-i.390189578.10117548962",
-        tag: "Star Wars",
-        nome: "A New Hope<br><br>",
-        imagem: "A-New-Hope-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Star-Wars-The-Empire-Strikes-Back-(O-Imp%C3%A9rio-Contra-Ataca)-i.390189578.6894229588",
-        tag: "Star Wars",
-        nome: "The Empire Strikes Back",
-        imagem: "The-Empire-Strikes-Back-min.jpg",
-        valor: "29,90"
-    },
-    {
-        link: "https://shopee.com.br/Caneca-Star-Wars-Return-of-The-Jedi-(O-Retorno-do-Jedi)-i.390189578.9476808769",
-        tag: "Star Wars",
-        nome: "Return of The Jedi",
-        imagem: "Return-of-the-Jedi-min.jpg",
-        valor: "29,90"
-    }
+const produtos = [
+  {
+    link: "",
+    tag: "HIMYM",
+    nome: "Barney's teaching",
+    imagem: "Barney's-teaching-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Marvel Comics",
+    nome: "Spider Man Photographer",
+    imagem: "spider-photo-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "HIMYM",
+    nome: "All I had to do",
+    imagem: "All-I-had-to-do-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Among Us",
+    nome: "Há 01 Impostor entre nós...",
+    imagem: "amongus-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Harry Potter",
+    nome: "Hogwarts Aquarela",
+    imagem: "aquarela-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "Strong in me Cuteness is!",
+    imagem: "Baby-Yoda-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "LOST",
+    nome: "Iniciativa DHARMA",
+    imagem: "Dharma-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Harry Potter",
+    nome: "Expresso Patronum!",
+    imagem: "Espresso-Patronum-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Friends",
+    nome: "Friends #2<br><br>",
+    imagem: "Friends-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Friends",
+    nome: "Friends Like<br><br>",
+    imagem: "Friends-Like-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Friends",
+    nome: "Friends <br><br>",
+    imagem: "FriendsLogo-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Dragon Ball",
+    nome: "Son Goku <br><br>",
+    imagem: "goku-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Grey's Anatomy",
+    nome: "Grey's Anatomy #2",
+    imagem: "Greys-Anatomy-1-min.jpg",
+    valor: "29,90",
+  },
+  {
+    tag: "Rick and Morty",
+    nome: " I Can Prove It Mathematically",
+    imagem: "I-can-prove-it-mathematically-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Grey's Anatomy",
+    nome: "It's a Beautiful Day",
+    imagem: "Its-a-beutiful-day-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "DC Comics",
+    nome: "Justice League ",
+    imagem: "Justice-League-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Naruto",
+    nome: "Kakashi Hatake ",
+    imagem: "Kakashi-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Dragon Ball",
+    nome: "Kame House<br><br>",
+    imagem: "Kame-House-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Naruto",
+    nome: "Lendário Jiraya",
+    imagem: "Lendario-Jiraya.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "LOST",
+    nome: "LOST<br><br>",
+    imagem: "LOST-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Rick and Morty",
+    nome: "Mr. Meeseeks<br><br>",
+    imagem: "mrMeesekes-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Rick and Morty",
+    nome: " Mug Riiiiick!<br><br>",
+    imagem: "Mug-Morty-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Naruto",
+    nome: "Naruto Evolution",
+    imagem: "Naruto-Evolution-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "New Trilogy<br><br>",
+    imagem: "New-Triology-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Dev",
+    nome: "No Coffe No Code",
+    imagem: "NoCoffeNoCode-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "LOST",
+    nome: "Not Penny's Boat",
+    imagem: "Not-Penny's-Boat-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Harry Potter",
+    nome: "O conto dos 3 irmãos",
+    imagem: "o-conto-dos-tres-irmãos-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Dev",
+    nome: "rogramador Criativo",
+    imagem: "ProgramadorCriativo-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Marvel Comics",
+    nome: "Homem Aranha Vintage",
+    imagem: "Spider-Man-Vintage-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Marvel Comics",
+    nome: "Stark Industries",
+    imagem: "Stark-Indrustries-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "Starbucks Grogu",
+    imagem: "StartBuksGrogu-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "DC Comics",
+    nome: "The Flash<br><br>",
+    imagem: "The-Flash-min-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Dragon Ball",
+    nome: "Personagens <br><br>",
+    imagem: "Todos-os-personagens-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Harry Potter",
+    nome: "Os três amigos<br><br>",
+    imagem: "TrioAmigos-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "Darth Troopers<br><br>",
+    imagem: "Troopers-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "HATE<br><br>",
+    imagem: "Hate-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "One Piece",
+    nome: "Trafalgar Law<br><br>",
+    imagem: "trafalgar-law-2-min.jpg",
+    valor: "29,90",
+  },
 
-
-
-]
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "A New Hope<br><br>",
+    imagem: "A-New-Hope-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "The Empire Strikes Back",
+    imagem: "The-Empire-Strikes-Back-min.jpg",
+    valor: "29,90",
+  },
+  {
+    link: "",
+    tag: "Star Wars",
+    nome: "Return of The Jedi",
+    imagem: "Return-of-the-Jedi-min.jpg",
+    valor: "29,90",
+  },
+];
